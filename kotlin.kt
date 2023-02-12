f4b24c4690b7b4a1b626ca11976d6ea71c4994be
=Raja Umair Yaseen <=rajaumairyaseen42@gmail.com>
2023-02-11 21:30:39 -0800

    Update kotlin.kt

--- kotlin.kt
+++ kotlin.kt
@@ -2,6 +2,11 @@ fun main(){
 var name:String="Raja Umair Yaseen"
   val id:Int=234242
   println("name is $name and id is $id")
-
-
+  var num1=5
+  var num2=5
+  val num3=add(num1,num2)
+println(num3)
+}
+private fun add(num1,num2):Int{
+return num1+num2
 }
